c75b2bbb56ab6f67e809cdb0efa78b93a4003159
agustinmrgo <agustinmrgo@gmail.com>
2017-05-25 14:05:22 -0300

    cambio11

--- EjemploABM1/AccesoDatos/RepositorioProductos.cs
+++ EjemploABM1/AccesoDatos/RepositorioProductos.cs
@@ -10,6 +10,7 @@ namespace EjemploABM1.AccesoDatos
         private SqlCommand ObtenerComando()
         {
             SqlConnection conn = new SqlConnection();
+
             //conn.ConnectionString = @"Server=AGUSTINDELL\SQLEXPRESS;Database=EjemploABM;Trusted_Connection=true;";
             conn.ConnectionString = @"Server=AGUSTIN-PC\SQLEXPRESS;Database=EjemploABM;Trusted_Connection=true;";
 
