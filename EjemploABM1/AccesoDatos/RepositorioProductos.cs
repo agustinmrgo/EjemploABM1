866dd78c88d428b7730ff71f6ac52fdf172c0977
agustinmrgo <agustinmrgo@gmail.com>
2017-05-25 00:30:35 -0300

    cambio10

--- EjemploABM1/AccesoDatos/RepositorioProductos.cs
+++ EjemploABM1/AccesoDatos/RepositorioProductos.cs
@@ -12,8 +12,11 @@ namespace EjemploABM1.AccesoDatos
             SqlConnection conn = new SqlConnection();
             //conn.ConnectionString = @"Server=AGUSTINDELL\SQLEXPRESS;Database=EjemploABM;Trusted_Connection=true;";
             conn.ConnectionString = @"Server=AGUSTIN-PC\SQLEXPRESS;Database=EjemploABM;Trusted_Connection=true;";
+
             var com = new SqlCommand();
+
             com.Connection = conn;
+
             return com;
         }
 
